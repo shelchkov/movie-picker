afccecb25376376603e909ab245d7b4ed25c8da9
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2023-04-14 14:19:16 +0400

    chore: add vercel adapter #1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
-import tailwind from "@astrojs/tailwind"
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 export default defineConfig({
   integrations: [preact(), tailwind()],
-  output: "server"
+  output: "server",
+  adapter: vercel()
 })
