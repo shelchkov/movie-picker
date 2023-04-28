567076c8cf14c09f97c49e95369a98a1f2b553b2
Yaroslav Shelchkov <shelchkovy@yandex.ru>
2023-04-28 10:32:56 +0400

    feat(web): add cast to movie card #9

--- src/components/MoviesList.tsx
+++ src/components/MoviesList.tsx
@@ -9,21 +9,29 @@ interface Props {
   error?: string
 }
 
+const imgHeight = 300
+
 export const MoviesList = ({ movies }: Props) => {
   if (!movies?.length) {
     return <ScreenContainer>Movies not found</ScreenContainer>
   }
 
   return (
-    <div>
+    <div class="flex flex-wrap justify-center">
       {movies.map((movie) => {
-        const { title } = getDataFromImageCaption(movie)
+        const { title, cast, alternativeTitle } = getDataFromImageCaption(movie)
+        const { width, height } = movie.primaryImage
+        const imgWidth = (imgHeight / height) * width
 
         return (
           <MovieCard
             imageSrc={movie.primaryImage.url}
             title={title}
             imageAlt={movie.primaryImage.caption.plainText}
+            // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
+            cast={cast || alternativeTitle}
+            imgWidth={imgWidth}
+            imgHeight={imgHeight}
             key={movie.id}
           />
         )
--- src/components/ui/MovieCard.tsx
+++ src/components/ui/MovieCard.tsx
@@ -4,11 +4,31 @@ interface Props {
   imageSrc: string
   title: string
   imageAlt?: string
+  cast?: string
+  imgWidth: number
+  imgHeight: number
 }
 
-export const MovieCard = ({ imageSrc, title, imageAlt }: Props) => (
-  <div class="flex p-2">
-    <img src={imageSrc} height={150} width={150} class="mr-2" alt={imageAlt} />
-    <p>{title}</p>
+export const MovieCard = ({
+  imageSrc,
+  title,
+  imageAlt,
+  cast,
+  imgWidth,
+  imgHeight,
+}: Props) => (
+  <div class="flex p-2 m-2 w-128 border border-zinc-500 rounded">
+    <img
+      src={imageSrc}
+      height={imgHeight}
+      width={imgWidth}
+      class="mr-2"
+      alt={imageAlt}
+    />
+
+    <div class="flex flex-col">
+      <p class="text-base">{title}</p>
+      {cast && <p class="mb-3 text-sm font-light">{cast}</p>}
+    </div>
   </div>
 )
